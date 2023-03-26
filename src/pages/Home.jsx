e509354c167aefebf75b8aee7fbc5218258bbfdf
Ethan Jung <ethan@abundowealth.com>
2023-03-26 16:55:43 -0500

    testing home page web scroll glitch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,24 +6,22 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main
-        className={`bg-[url('/img/cover.webp')] bg-no-repeat bg-cover bg-center bg-fixed min-h-screen flex flex-col justify-center`}
-          >
-              <div className="h-screen absolute bg-gray-900 bg-opacity-[0.15] w-screen"></div>
-        <div className="relative px-6 py-8 lg:py-14 lg:px-8 flex items-center">
-          <div className="mx-auto text-center flex flex-col space-y-6 mt-6">
-            <h1 className="text-3xl sm:text-5xl lg:text-8xl font-lato text-white tracking-widest font-thin">
-              ETHAN &amp; REBECA
-            </h1>
-            <h2 className="text-lg sm:text-xl lg:text-2xl text-white uppercase tracking-widest font-lato font-thin">
-              29 July 2023
-            </h2>
+      <div className="flex flex-col min-h-screen">
+        <main className="bg-[url('/img/cover.webp')] bg-no-repeat bg-cover bg-center bg-fixed flex-grow flex flex-col justify-center">
+          <div className="h-screen absolute bg-gray-900 bg-opacity-[0.15] w-screen"></div>
+          <div className="relative px-6 py-8 lg:py-14 lg:px-8 flex items-center">
+            <div className="mx-auto text-center flex flex-col space-y-6 mt-6">
+              <h1 className="text-3xl sm:text-5xl lg:text-8xl font-lato text-white tracking-widest font-thin">
+                ETHAN &amp; REBECA
+              </h1>
+              <h2 className="text-lg sm:text-xl lg:text-2xl text-white uppercase tracking-widest font-lato font-thin">
+                29 July 2023
+              </h2>
+            </div>
           </div>
-        </div>
-        <div className="absolute bottom-0 w-full">
-          <Footer />
-        </div>
-      </main>
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
