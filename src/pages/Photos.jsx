e391b125b93fe0026300b3ef3335a24ab4096bd9
Ethan Jung <ethan@abundowealth.com>
2023-03-26 17:25:19 -0500

    added more photos

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -9,10 +9,19 @@ const files = [
     source:
       "https://res.cloudinary.com/dov31rv0v/image/upload/v1678644794/wedding%20website/photo_gallery/TCR66516_dfdxrm.webp",
   },
+  { source: 'https://res.cloudinary.com/dov31rv0v/image/upload/v1679869361/wedding%20website/photo_gallery/IMG_20220928_100017_413_x7fyy5.webp' },
+  {
+    source: "https://res.cloudinary.com/dov31rv0v/image/upload/v1679869346/wedding%20website/photo_gallery/DSC04831_ense9t.jpg"
+  },
   {
     source:
       "https://res.cloudinary.com/dov31rv0v/image/upload/v1678644796/wedding%20website/photo_gallery/TCR66537-_1__ynvgql.webp",
   },
+  {
+    source: 'https://res.cloudinary.com/dov31rv0v/image/upload/v1679869412/wedding%20website/photo_gallery/DSC04929_cosjm9.jpg'
+  },
+  { source: 'https://res.cloudinary.com/dov31rv0v/image/upload/v1679869404/wedding%20website/photo_gallery/DSC04688_mcmb64.jpg' },
+  
   {
     source:
       "https://res.cloudinary.com/dov31rv0v/image/upload/v1678644930/wedding%20website/photo_gallery/TCR66534_fnqh7z.webp",
@@ -39,7 +48,7 @@ export default function Example() {
               <img
                 src={file.source}
                 alt=""
-                className="pointer-events-none object-cover group-hover:opacity-75"
+                className="pointer-events-none object-cover "
               />
             </div>
           </li>
